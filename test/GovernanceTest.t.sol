1552519193d3a6a9d44ed116e3b246d4f0fcc4db
dev_apus <folorunsopraise580@gmail.com>
2024-06-22 12:26:21 +0100

    Governance test completed

--- test/GovernanceTest.t.sol
+++ test/GovernanceTest.t.sol
@@ -8,7 +8,9 @@ import {
     Governance,
     Governance__VotingExpired,
     Governance__InsufficientVotingPower,
-    Governance__AlreadyVoted
+    Governance__AlreadyVoted,
+    Governance__VotingNotExpired,
+    Governance__AlreadyExecuted
 } from "../src/Governance.sol";
 
 contract GovernanceTest is Test {
@@ -61,5 +63,75 @@ contract GovernanceTest is Test {
         vm.prank(user2);
         vm.expectRevert(Governance__AlreadyVoted.selector);
         governance.vote(proposalId, true);
+    }
+
+    function testReturnCorrectProposalId() public {
+        uint256 proposalId = governance.createProposal("Second proposal", block.timestamp + (60 * 60), 4);
+        assertEq(proposalId, 4);
+    }
+
+    function testCannotExecuteProposalBeforeDeadLine() public {
+        uint256 proposalId = governance.createProposal("Second proposal", block.timestamp + (60 * 60), 4);
+        vm.expectRevert(Governance__VotingNotExpired.selector);
+        governance.executeProposal(proposalId);
+    }
+
+    function testCannotExecuteProjectExecuted() public {
+        uint256 proposalId = governance.createProposal("Second proposal", block.timestamp + (60 * 60), 4);
+        vm.prank(user1);
+        governance.vote(proposalId, true);
+        vm.prank(user2);
+        governance.vote(proposalId, true);
+        vm.warp(block.timestamp + (60 * 60 * 2));
+        vm.stopPrank();
+        governance.executeProposal(proposalId);
+        vm.expectRevert(Governance__AlreadyExecuted.selector);
+        governance.executeProposal(proposalId);
+    }
+
+    function testReturnsProposalStruct() public {
+        uint256 proposalId = governance.createProposal("Second proposal", block.timestamp + (60 * 60), 4);
+        Governance.Proposals memory proposal = governance.getProposal(proposalId);
+
+        assertEq(proposal.description, "Second proposal");
+        assertEq(proposal.votesFor, 0);
+        assertEq(proposal.votesAgainst, 0);
+        assertEq(proposal.executed, false);
+    }
+
+    function testReturnsVoteFor() public {
+        uint256 proposalId = governance.createProposal("Second proposal", block.timestamp + (60 * 60), 4);
+        vm.prank(user1);
+        governance.vote(proposalId, true);
+        vm.prank(user2);
+        governance.vote(proposalId, false);
+        uint256 voteFor = governance.getVotesFor(proposalId);
+
+        assertEq(voteFor, 20000);
+    }
+
+    function testReturnsVoteAgainst() public {
+        uint256 proposalId = governance.createProposal("Second proposal", block.timestamp + (60 * 60), 4);
+        vm.prank(user1);
+        governance.vote(proposalId, true);
+        vm.prank(user2);
+        governance.vote(proposalId, false);
+        uint256 voteAgainst = governance.getVotesAgainst(proposalId);
+
+        assertEq(voteAgainst, 10000);
+    }
+
+    function testReturnsDescription() public {
+        uint256 proposalId = governance.createProposal("Second proposal", block.timestamp + (60 * 60), 4);
+        string memory description = governance.getDescription(proposalId);
+
+        assertEq(description, "Second proposal");
+    }
+
+    function testReturnsExecution() public {
+        uint256 proposalId = governance.createProposal("Second proposal", block.timestamp + (60 * 60), 4);
+        bool execution = governance.getExecution(proposalId);
+
+        assertEq(execution, false);
     }
 }
